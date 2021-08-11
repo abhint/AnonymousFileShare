229352143a26ba0c1e0950c39fc61d5524e2cd30
AbhijithNT <abhijithnt64@gmail.com>
2021-08-12 00:00:45 +0530

    /ping Command

--- app/commands/index.ts
+++ app/commands/index.ts
@@ -1,8 +1,10 @@
+import bot from "../bot";
 import { onStart } from "./start";
 import { onHelp } from "./help";
-import bot from "../bot";
+import { onPing } from "./ping";
 
 export const onCommands = async () => {
   bot.use(onStart);
   bot.use(onHelp);
+  bot.use(onPing);
 };
