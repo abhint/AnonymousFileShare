51016fd6b8a620a20419776f8958699e31640b1e
AbhijithNT <abhijithnt64@gmail.com>
2021-08-14 23:51:46 +0530

    :sparkles:

---
+++ app/commands/messages.ts
@@ -0,0 +1,5 @@
+export const onText = {
+  start:
+    "\n\nSend location, video, audio, document, voice, photo, and text This bot will remove forward information.",
+  help: "Repo Source: https://github.com/AbhijithNT/AnonymousFileShare\nAdmin: @thankappan369\nMade with ❤️ by Telegram",
+};
--- app/commands/ping.ts
+++ app/commands/ping.ts
@@ -2,7 +2,7 @@ import { Composer } from "telegraf";
 
 export const onPing = Composer.command("ping", async (msg) => {
   let st: number = Date.now();
-  let msg_id = await msg.telegram.sendMessage(msg.chat.id, `pong!`);
+  let msg_id = await msg.telegram.sendMessage(msg.chat.id, `Pong!`);
   let ms: number = Date.now() - st;
 
   await msg.telegram.editMessageText(
