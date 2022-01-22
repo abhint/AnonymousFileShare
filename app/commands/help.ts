226db78228ffe995133b5f4b98e54605da0b5ab2
AbhijithNT <abhijithnt64@gmail.com>
2022-01-22 20:50:02 +0530

    set parse mode

--- app/commands/help.ts
+++ app/commands/help.ts
@@ -2,5 +2,7 @@ import { Composer } from "telegraf";
 import { onText } from "./messages";
 
 export const onHelp = Composer.command("help", async (msg) => {
-  msg.telegram.sendMessage(msg.chat.id,`${onText.help}`);
+  msg.telegram.sendMessage(msg.chat.id, `${onText.help}`, {
+    parse_mode: "Markdown",
+  });
 });
