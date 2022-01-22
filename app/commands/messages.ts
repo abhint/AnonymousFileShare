89ec5bbadb6a170c52b46431c6348f098bc6d6b9
AbhijithNT <abhijithnt64@gmail.com>
2022-01-22 21:03:07 +0530

    italic text and spelling

--- app/commands/messages.ts
+++ app/commands/messages.ts
@@ -1,5 +1,5 @@
 export const onText = {
   start:
-    "\n\nSend location, video, audio, document, voice, photo, and text This bot will remove forward information.",
-  help: "Repo Source: https://github.com/abhint/AnonymousFileShare\nAdmin: [Abhijith](tg://user?id=429320566)\nMade with ❤️ by Telegram",
+    "\n\n_Send location, video, audio, document, voice, photo, and text This bot will remove forward information._",
+  help: "Repo: https://github.com/abhint/AnonymousFileShare\nAdmin: [Abhijith](tg://user?id=429320566)\nMade with ❤️ by Telegram",
 };
