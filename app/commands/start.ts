fbe65dd1f2a40c078f6a0c9b4b523b96e10fb6b8
AbhijithNT <abhijithnt64@gmail.com>
2022-01-22 20:52:17 +0530

    parse mode set Markdown and username

--- app/commands/start.ts
+++ app/commands/start.ts
@@ -3,18 +3,18 @@ import { onText } from "./messages";
 export const onStart = Composer.command("start", async (msg) => {
   msg.telegram.sendMessage(
     msg.chat.id,
-    `Hi <b>${msg.message.from.first_name.replace(/[<>/]/g, "")} !</b>${
+    `Hi *${msg.message.from.first_name.replace(/[<>/]/g, "")} !*${
       onText.start
     }`,
     {
-      parse_mode: "HTML",
+      parse_mode: "Markdown",
       reply_markup: {
         inline_keyboard: [
           [{ text: "Developer", url: "tg://user?id=429320566" }],
           [
             {
               text: "Source",
-              url: "https://github.com/AbhijithNT/AnonymousFileShare/",
+              url: "https://github.com/abhint/AnonymousFileShare",
             },
           ],
         ],
